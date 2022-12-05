a4fa9738e3591359e1b61b276511b1830006992f
DjamalovMuhammadAli <djamalov.muhammad.ali7@gmail.com>
2022-12-05 18:07:03 +0500

    .

--- project/article/models.py
+++ project/article/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Article(models.Model):
   created_date = models.DateTimeField(auto_now=True)
   name = models.CharField(max_length=200)
-  text = models.text_field()
+  text = models.TextField()
+
+  def __str__(self):
+    return '%s:.%s-%s'%(self.created_date, self.name, self.text) 
--- project/project/urls.py
+++ project/project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# My
+from api.article.urls import *
 
 urlpatterns = [
+    path('', include('article.urls')),
     path('admin/', admin.site.urls),
 ]
