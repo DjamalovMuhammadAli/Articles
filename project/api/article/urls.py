0f9532810cec9f2a87a0efffc65003e312a75e85
DjamalovMuhammadAli <djamalov.muhammad.ali7@gmail.com>
2022-12-06 20:53:32 +0500

    success_update

--- project/api/article/urls.py
+++ project/api/article/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.ArticleListView.as_view(), name='home'),
     path('detail/<int:pk>', views.DetailDetailView.as_view(), name='detail_page'),
     path('edit-page', views.edit_page, name='edit_page'),
-]
+    path('update-page/<int:pk>', views.update_page, name='update_page'),
+]   
--- project/api/article/views.py
+++ project/api/article/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # My
 from article.models import Article
 from django.views.generic import ListView, DetailView
+from article.forms import ArticleForm
 
 
 class ArticleListView(ListView):
@@ -18,8 +19,39 @@ class DetailDetailView(DetailView):
 
 
 def edit_page(request):
+  success = False
+  if request.method == 'POST':
+    form = ArticleForm(request.POST)
+    if form.is_valid():
+      form.save()
+      success = True
+
   template = 'edit_page.html'
   context = {
-    'list_articles': Article.objects.all().order_by('-id')
+    'list_articles': Article.objects.all().order_by('-id'),
+    'form': ArticleForm(),
+    'success': success
   }
+  return render(request, template, context)
+
+def update_page(request, pk):
+
+  success_update = False
+
+  get_article = Article.objects.get(pk=pk)
+
+  if request.method == 'POST':
+    form = ArticleForm(request.POST, instance = get_article)
+    if form.is_valid():
+      form.save()
+      success_update = True
+  
+  template = 'edit_page.html'
+  context = {
+    'get_article': get_article,
+    'update':True,
+    'form':ArticleForm(instance = get_article),
+    'success_update':success_update
+  }
+
   return render(request, template, context)
