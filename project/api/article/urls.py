db92050499cb6af1cb7fee129aa6ae31037e36b6
DjamalovMuhammadAli <djamalov.muhammad.ali7@gmail.com>
2022-12-06 06:02:37 +0500

    .

--- project/api/article/urls.py
+++ project/api/article/urls.py
@@ -1,1 +1,10 @@
+from django.urls import path
 
+# My
+from api.article import views 
+
+urlpatterns = [
+    # path('articles/', views.articles),
+    path('', views.ArticleListView.as_view()),
+    path('detail/<int:id>', views.detail)
+]
--- project/api/article/views.py
+++ project/api/article/views.py
@@ -1,2 +1,35 @@
 from django.shortcuts import render
 
+# My
+from article.models import Article
+from django.views.generic import ListView, DetailView
+
+# def get_article(request):
+#   list_articles = Article.objects.all()
+#   context = {
+#     'name': 'Ali',
+#     'text': list_articles,
+#   }
+#   template = 'articles.html'
+#   return render(request, template, context)
+
+
+class ArticleListView(ListView):
+  model = Article
+  template_name = 'articles.html'
+  context_object_name = 'list_articles'
+
+
+# def get_details(request, id):
+#   get_articles = Article.objects.get(id=id)
+#   context = {
+#     'get_articles': get_articles,
+#   }
+#   template = 'detail.html'
+#   return render(request, template, context)
+
+
+class DetailDetailView(DetailView):
+  model = Article
+  template_name = 'detail.html'
+  context_object_name = 'get_article'
